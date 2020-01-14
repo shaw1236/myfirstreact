dd30ccad057ae06e2250ed1236ee1ed87a1369f9
Simon Li <shaw1236@gmail.com>
2020-01-14 11:53:21 -0500

    Code Refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // Purpose: react app demo
 // Author : Simon Li
 // Date   : Jan 11, 2020
+
+// $npm install -g create-react-app
+// $npx create-react-app myfirstreact
+
+// $npm start
+
+// $npm build
+// $npm install -g serve
+// $serve -s build
+// serve -s build -l 4000
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Car from './car/Car.js';
@@ -40,7 +52,7 @@ class Main extends React.Component {
           default:
                break;
       } 
-      ReactDOM.render(<objWhich />, document.getElementById('root'));
+      ReactDOM.render(objWhich, document.getElementById('root'));
    }
 
    render = () => {
