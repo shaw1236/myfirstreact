4abde247097c336be8a5a02ec17ed703b249074f
Simon Li <shaw1236@gmail.com>
2020-01-14 16:38:21 -0500

    split the main file

--- src/index.js
+++ src/index.js
@@ -10,66 +10,11 @@
 // $npm build
 // $npm install -g serve
 // $serve -s build
-// serve -s build -l 4000
+// $serve -s build -l 4000
 
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './car/Car.js';
-import MyForm from './form/MyForm.js';
-import Football from './football/Football.js';
-import MyHeader from './header/MyHeader';
-
-class Main extends React.Component {
-   constructor(props) {
-       super(props);
-       this.state = {
-          which: ''
-       };
-   }
-   mySelectHandler = (event) => {
-      let value = event.target.value;
-
-      this.setState({which: value});
-      this.show(value);
-   }
-
-   show = (which) => {
-      let objWhich = null;
-      switch(which) {
-          case 'Car':
-               objWhich = <Car />;
-               break;
-          case 'Football':
-               objWhich = <Football />;
-               break; 
-          case 'Form':
-               objWhich = <MyForm />;
-               break; 
-          case 'Header':
-               objWhich = <MyHeader />;
-               break; 
-          default:
-               break;
-      } 
-      ReactDOM.render(objWhich, document.getElementById('root'));
-   }
-
-   render = () => {
-       let header = <h3>Please choose one</h3>;  
-       return (
-         <div>
-             {header}
-             <select value={this.state.mycar} onChange={this.mySelectHandler}>
-              <option value="#">Pick one</option>
-              <option value="Car">Car</option>
-              <option value="Form">Form</option>
-              <option value="Football">Football</option>
-              <option value="Header">Header</option>
-            </select>
-         </div>
-       );
-   }
-}
+import Main from './App/Main';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
